d8b4b5b99d3b3532dcea7c3493b8bf86cb379a78
ikabeee <carlglz30@gmail.com>
2024-12-08 18:26:18 -0500

    First commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+    .setTitle('Boxeats API ')
+    .setDescription('Final project SOA')
+    .setVersion('1.0')
+    .addTag('MisteryBoxes')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
